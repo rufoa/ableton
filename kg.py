e4c0b618e3d13ecb8546c179fb34ac502993ed81
rufo <rufus@rufus.al>
2026-06-29 05:00:39 +0100

    hwid validation

--- kg.py
+++ kg.py
@@ -15,8 +15,16 @@ EDITIONS = {
     "Suite": 2,
 }
 
+
+def parse_hwid(s: str) -> str:
+    s = s.upper().replace("-", "")
+    if re.fullmatch(r"[0-9A-F]{24}", s):
+        return "-".join(s[i:i + 4] for i in range(0, 24, 4))
+    raise argparse.ArgumentTypeError("Expected hardware ID like 1111-1111-1111-1111-1111-1111, not {}".format(s))
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--hwid", help="Your hardware code", required=True)
+parser.add_argument("-i", "--hwid", help="Your hardware code", type=parse_hwid, required=True)
 parser.add_argument("-o", "--output", help="Authorization file", default="Authorize.auz")
 parser.add_argument("-v", "--version", help="Ableton Live version", type=int, choices=range(9, 13), default=12)
 parser.add_argument("-e", "--edition", help="Ableton Live edition", type=str.capitalize, choices=EDITIONS, default="Suite")
@@ -104,11 +112,6 @@ team_r2r_key = construct_key(
     x=0xc369ea757b46484d1df3819cc4183f6f9a9bcf3c
 )
 
-hwid = args.hwid.upper()
-if len(hwid) == 24:
-    hwid = "-".join(hwid[i:i+4] for i in range(0, 24, 4))
-assert re.fullmatch(r"([0-9A-F]{4}-){5}[0-9A-F]{4}", hwid), f"Expected hardware ID like 1111-1111-1111-1111-1111-1111, not {hwid}"
-
-lines = generate_all(team_r2r_key, args.edition, args.version, hwid)
+lines = generate_all(team_r2r_key, args.edition, args.version, args.hwid)
 with open(args.output, mode="w", newline="\n") as f:
     f.write("\n".join(lines))
